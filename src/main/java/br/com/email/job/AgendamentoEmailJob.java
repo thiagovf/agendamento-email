dc57338322c942f786d331fea75f8ba0b39caba6
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-08-20 08:47:48 -0300

    Transformando em singleton

--- src/main/java/br/com/email/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/email/job/AgendamentoEmailJob.java
@@ -3,18 +3,18 @@ package br.com.email.job;
 import java.util.List;
 
 import javax.ejb.Schedule;
-import javax.ejb.Stateless;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
 
 import br.com.email.entidade.AgendamentoEmail;
 import br.com.email.servico.AgendamentoEmailServico;
 
-@Stateless
+@Singleton
 public class AgendamentoEmailJob {
-
+	
 	@Inject
 	private AgendamentoEmailServico agendamentoEmailServico;
-	
+
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmails() {
 		List<AgendamentoEmail> listaAgendamentos = agendamentoEmailServico.listarPorNaoAgendado();
